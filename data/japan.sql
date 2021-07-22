52bfe1d5744a260c4c989afa078c229e3d0dbfec
mathieuelisa <mathieu.elisa@gmail.com>
2021-07-22 20:47:23 +0200

    Created data.sql

--- data/japan.sql
+++ data/japan.sql
@@ -1,5 +1,38 @@
 BEGIN;
 
+DROP TABLE IF EXISTS "list", "card";
+
+CREATE TABLE IF NOT EXISTS "list" (
+    "id" serial PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "card" (
+    "id" serial PRIMARY KEY,
+    "name_FR" TEXT NOT NULL,
+    "name_EN" TEXT NOT NULL,
+    "name_JP" TEXT NOT NULL,
+    "list_id" INT NOT NULL REFERENCES "list"("id") ON DELETE CASCADE,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NULL
+);
+
+INSERT INTO "list" ("id", "name") VALUES
+(1, 'House'),
+(2, 'Kitchen'),
+(3, 'Bedroom'),
+(4, 'Animaux');
+
+INSERT INTO "card" ("id", "name_FR","name_EN","name_JP","list_id") VALUES
+(1, 'Maison', 'House', 'Uchi', 1),
+(2, 'Ascenseur','Elevato','Erebeetaa', 1),
+(3, 'Four','Four','Oobun', 2),
+(4, 'Oreiller','Hear','Makura', 1),
+(5, 'Refrigerateur','Fridge', 'Reizouko', 2),
+(6, 'Lit','Bed', 'Beddo', 3),
+(7, 'Araignée','Spider', 'Kumo', 4);
 
 
 COMMIT;
