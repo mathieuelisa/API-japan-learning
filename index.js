b63dcfce3ef28672f3e6b56dd7db067fa801991f
mathieuelisa <mathieu.elisa@gmail.com>
2021-10-12 16:49:24 +0200

    Update

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.use(cors("*"));
 app.use(router);
 
@@ -16,6 +15,10 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
+//   console.log(`Server started`);
+// });
+
+app.listen(process.env.PORT, () => {
   console.log(`Server started`);
 });
