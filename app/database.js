26498bd5cb5f20c0062bae843ee2118b61f748d4
mathieuelisa <mathieu.elisa@gmail.com>
2021-10-12 16:10:07 +0200

    Adding heroku

--- app/database.js
+++ app/database.js
@@ -1,5 +1,12 @@
 const { Sequelize } = require("sequelize");
 
+if (process.env.NODE_ENV === "production") {
+  // config pour la version de prod heroku
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
 const sequelize = new Sequelize(process.env.PG_URL, {
   define: {
     underscored: false,
